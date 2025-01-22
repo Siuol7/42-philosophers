723fc6d2d58cca4034f5621a7db8f2a9447358a5
Siuol D <nguyencaotan03072000@gmail.com>
2025-01-23 01:39:30 +0700

    Update struct

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -6,7 +6,7 @@
 /*   By: caonguye <caonguye@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 15:38:21 by caonguye          #+#    #+#             */
-/*   Updated: 2025/01/21 00:34:56 by caonguye         ###   ########.fr       */
+/*   Updated: 2025/01/23 01:36:50 by caonguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,11 @@
 typedef struct s_dinner
 {
 	pthread_t		philo_cnt;
-	pthread_mutex_t	*forks;
+	pthread_t		fork_cnt;
+	pthread_mutex_t	*thread;
+	pthread_mutex_t	*status;
+	
+	pthread_mutex_t	**forks_lst;
 	pthread_mutex_t	status;
 }	t_dinner;
 
@@ -33,6 +37,9 @@ typedef struct s_philo
 	unsigned int	time_to_die;
 	unsigned int	time_to_sleep;
 	unsigned int	meals_cnt;
+	pthread_mutex_t	*left_fork;
+	pthread_mutex_t	*right_fork;
+
 }	t_philo;
 
 #endif

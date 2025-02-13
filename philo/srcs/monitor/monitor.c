d94fbc61208b2a93370f76364bf60308184556dc
Siuol7 <nguyencaotan03072000@gmail.com>
2025-02-13 05:15:20 +0200

    Update monitor frame

--- philo/srcs/monitor/monitor.c
+++ philo/srcs/monitor/monitor.c
@@ -6,53 +6,68 @@
 /*   By: caonguye <caonguye@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 17:04:12 by caonguye          #+#    #+#             */
-/*   Updated: 2025/02/13 04:46:59 by caonguye         ###   ########.fr       */
+/*   Updated: 2025/02/13 05:14:56 by caonguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <philo.h>
 
-static int	error_signal(t_dinner *table)
+static int	error_track(t_dinner *table)
 {
 
 }
 
-static int	over_signal(t_dinner *table)
+static int	over_track(t_dinner *table, int i)
 {
-
-}
-
-static int	done_signal(t_dinner *table)
-{
-
-}
-static int	stop_call(t_dinner *table)
-{
-	int	i;
-
-	i = -1;
-	while (++i < table->philo_cnt)
+	if (!lock_mutex(table->philo[i].philo_key))
+		return (-1);
+	if (current() > table->philo[i].next_meal)
 	{
-		if (!lock_mutex(table->total_meals_key))
-			return (0);
-		if (table->philos_done == table->philo_cnt)
-			feast_done(table);
-		if (!unlock_mutex(table->total_meals_key))
-			return (0);
-		if (!lock_mutex(table->philo[i].philo_key))
-			return (0);
-		if (current() > table->philo[i].next_meal)
-		{
-			table->philo_death = 1;
-			feast_over(table);
-		}
-		if (!unlock_mutex(table->philo[i].philo_key))
-			return (0);
+		table->philo_death = 1;
+		feast_over(table);
+		return (1);
 	}
-	return (1);
+	if (!unlock_mutex(table->philo[i].philo_key))
+		return (-1);
+	return (0);
+}
+
+static int	done_track(t_dinner *table)
+{
+	if (!lock_mutex(table->total_meals_key))
+		return (-1);
+	if (table->philos_done == table->philo_cnt)
+	{
+		feast_done(table);
+		return (1);
+	}
+	if (!unlock_mutex(table->total_meals_key))
+		return (-1);
+	return (0);
 }
 
 void	tracking(t_dinner *table)
 {
+	int	i;
+	int error_signal;
+	int over_signal;
+	int done_signal;
 
+	i = 0;
+	while (++i < table->philo_cnt)
+	{
+		error_signal = error_track(table);
+		over_signal = over_track(table, i);
+		done_signal = done_track(table);
+
+		if (!error_signal || !over_signal || !done_signal)
+			i++;
+		if (error_signal == -1 || over_signal == -1
+					|| done_signal == -1)
+			return (-1);
+		if (error_signal == 1 || over_signal == 1
+					|| done_signal == 1)
+			return (1);
+	}
+	return (1);
 }

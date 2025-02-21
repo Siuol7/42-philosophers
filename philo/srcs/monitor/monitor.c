0c55c2907aaff23b0e31864deb17da8139523e0d
Siuol7 <nguyencaotan03072000@gmail.com>
2025-02-21 02:15:37 +0200

    Add mutex lock for death status

--- philo/srcs/monitor/monitor.c
+++ philo/srcs/monitor/monitor.c
@@ -6,7 +6,7 @@
 /*   By: caonguye <caonguye@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 17:04:12 by caonguye          #+#    #+#             */
-/*   Updated: 2025/02/19 02:57:17 by caonguye         ###   ########.fr       */
+/*   Updated: 2025/02/21 02:12:47 by caonguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,9 @@ static int	over_track(t_dinner *table, size_t i)
 
 	if (current() - table->philo[i].last_meal >= table->time_to_die)
 	{
+		pthread_mutex_lock(&table->status_key);
 		table->philo_death = 1;
+		pthread_mutex_unlock(&table->status_key);
 		feast_over(&table->philo[i]);
 		pthread_mutex_unlock(table->philo[i].philo_key);
 		return (1);
